f6cfa045ef5299b0396a644e97eed23b154b003d
tca85 <thiago.alves.85@gmail.com>
2016-01-09 10:33:37 -0500

    building controller

--- src/main/java/tca85/com/github/spring/mvc/crud/controller/CreateController.java
+++ src/main/java/tca85/com/github/spring/mvc/crud/controller/CreateController.java
@@ -1,8 +1,30 @@
 package tca85.com.github.spring.mvc.crud.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 
+ * @author tca85
+ *
+ */
 @Controller
 public class CreateController {
+
+	//---------------------------------------------------------------------------------------------
+	/**
+	 * This method is called based on URL that user typed on the browser
+	 * and return the html page name (CreateBill.html)
+	 * 
+	 * http://localhost:8080/create/bill
+	 * 
+	 * @return
+	 */
+	@RequestMapping("create/bill")
+	public String create(){	
+		return "CreateBill";
+	}
+	
+	//---------------------------------------------------------------------------------------------
 
 }
