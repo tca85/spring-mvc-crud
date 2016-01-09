0b2f4e334de848603b53d856f14ad01b037b66a1
tca85 <thiago.alves.85@gmail.com>
2016-01-09 10:09:33 -0500

    spring boot + bootstrap

---
+++ README.md
@@ -0,0 +1,9 @@
+CRUD example with Spring MVC using Spring Tool Suite 3.7.2
+
+- Spring Boot 1.3.1
+- Bootstrap v3.3.6
+- Java 8
+- Maven
+- MySQL
+- Thymeleaf
+- Tomcat (could be Glassfish or Wildfly)
---
+++ src/main/java/spring/mvc/crud/tca85/com/github/ServletInitializer.java
@@ -0,0 +1,13 @@
+package spring.mvc.crud.tca85.com.github;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringMvcCrudApplication.class);
+	}
+
+}
